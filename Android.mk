c47d04bb729f3eb6c2482da6652df8e1c22029af
AdarshGrewal <adarshgrewal@gmail.com>
2026-01-20 23:24:54 +0530

    peridot: Include firmware
    Change-Id: I7b87507ad9931eee1fab4f9a4aa3c7f90ab962ee

--- Android.mk
+++ Android.mk
@@ -3,3 +3,32 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),peridot)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,865a1e64969f7928751c157bc799615a793ce1f4)
+$(call add-radio-file-sha1-checked,radio/aop.img,a54917426cc1d340f65acc97ecd1c7919cce79b8)
+$(call add-radio-file-sha1-checked,radio/aop_config.img,ea1cb477a39f48b85d4aaa92208e37a05e31321c)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,bbdb3a9aab9becfb3249d120f914681211187c91)
+$(call add-radio-file-sha1-checked,radio/countrycode.img,87c9f7b390040342c338039d69de840d94842d31)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,41b4ebf380a398a68c5d55268547dd2095a0b34b)
+$(call add-radio-file-sha1-checked,radio/cpucp_dtb.img,e789ed96274974c5a21746902db9cbfd30442623)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,6e458a2fe41c49a97edb948b249236f11a657ca4)
+$(call add-radio-file-sha1-checked,radio/dsp.img,af01b0f0ab17e08534c6cbd86709dd4c4e813065)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,f70dba47cf9f3b316365b308be4d48f49bda5b98)
+$(call add-radio-file-sha1-checked,radio/hyp.img,0c5c3d6525184d769a801fd9176277df0ae48a56)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,52fd20f8eab7efb5747570a28b706d8b8e06c9d2)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,ba1c40ddb0c98dac7bd63c39939500388693a2cc)
+$(call add-radio-file-sha1-checked,radio/modem.img,7532f40b1a4c9304cd215cc93516e803269fe6d2)
+$(call add-radio-file-sha1-checked,radio/modemfirmware.img,2321bc92e2cab18d59a9d75c1ac3b26bfa2a4c27)
+$(call add-radio-file-sha1-checked,radio/multiimgqti.img,85c6e03d0dd54630f5017c9c83517fbe19ad6af1)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,68791c9438713efb98918e204dc4895b6624503f)
+$(call add-radio-file-sha1-checked,radio/shrm.img,0646b385e5cdb3dfbf725c99aa3011c722bbe4d4)
+$(call add-radio-file-sha1-checked,radio/tz.img,a54311cf5a072e128978d79e2a38c75317db7ea8)
+$(call add-radio-file-sha1-checked,radio/uefi.img,a00179be520540887d96db366e59a1ba12f0c07b)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,76c4a2f6a4b1967f4fbf876c07b9db4d77dbd88d)
+$(call add-radio-file-sha1-checked,radio/xbl.img,5ed31e8a9003ca702a8bef279f25473bfb505558)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,2e58d4f1cef305fbc7f1b807a9e503e771e4f885)
+$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,7297ed892cdcc6957cceb677962d9b5d5fc3db51)
+
+endif
