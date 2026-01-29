7e0e489dffcbe0baa4754066f3900dd6c8041d3e
AdarshGrewal <adarshgrewal@gmail.com>
2026-01-29 16:53:58 +0530

    peridot: configs: Remove shoebox effect
    Change-Id: I891eb868a3ca165d95ab9b13e54ffa623b32b11e

--- peridot-vendor.mk
+++ peridot-vendor.mk
@@ -1669,7 +1669,6 @@ PRODUCT_PACKAGES += \
     sensors.qsh \
     libasphere \
     libquasar \
-    libshoebox \
     vendor.display.color@1.0 \
     vendor.display.color@1.1 \
     vendor.display.color@1.2 \
